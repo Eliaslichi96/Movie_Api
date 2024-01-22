5381a2a12fb6702a769b2a3655b503c35e26f94f
Eliaslichi96 <146300075+Eliaslichi96@users.noreply.github.com>
2024-01-21 20:43:41 -0500

    2.5 postman error
    2.5 postman error

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 morgan = require('morgan'),
-fs = require('fs'), 
+fs = require('fs'),
 bodyParser = require('body-parser'),
 path = require('path'),
 uuid = require('uuid');
-
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,14 +11,23 @@ app.use(bodyParser.json());
 
 let users = [
   {
-  id: 1,
-  name: "Kim",
-  favoriteMovies: []
+    id:1,
+    name: "Kim",
+    favMovies: []
+},
+{
+  id:2,
+    name: "Secondloco",
+    favMovies: ['The Dark Knight']
+  },
+  {
+    id:3,
+    name: "Thirdloco",
+    favMovies: ['Inception']
   }
-
 ]
 
-let topMovies = [
+let Movies = [
     {
       'Title': 'Harry Potter Series',
       'Description': '',
@@ -36,132 +44,106 @@ let topMovies = [
     },
     {
       'Title': 'Lord of the Rings: The Return of the King',
-      'Description': '',
+      'Description': 'The final installment of the epic fantasy trilogy where the fellowship faces its ultimate challenge in the battle for Middle Earth.',
       'Genre': {
-        'Name':'',
-        'Description':''
+        'Name': 'Fantasy',
+        'Description': 'Fantasy genre involving epic quests and magical elements.'
       },
       'Director': {
-        'Name': '',
-        'Bio': '',
-        'Birth': ''
+        'Name': 'Peter Jackson',
+        'Bio': 'Peter Jackson is a New Zealand film director, producer, and screenwriter. He is best known for his work on the film adaptations of J.R.R. Tolkien\'s novels.',
+        'Birth': 'October 31, 1961'
       }
     },
     {
-      'Title': "The Dark Knight",
-      'Description': '',
-      'Genre': {
-        'Name':'',
-        'Description':''
-      },
-      'Director': {
-        'Name': '',
-        'Bio': '',
-        'Birth': ''
-      }
+      'Title': 'The Dark Knight',
+  'Description': 'In this superhero film, Batman faces the Joker, a criminal mastermind who seeks to create chaos in Gotham City. The battle between the two forces Batman to confront his own moral limits.',
+  'Genre': {
+    'Name': 'Action',
+    'Description': 'Action film involving intense conflicts, often with a central superhero or protagonist.'
+  },
+  'Director': {
+    'Name': 'Christopher Nolan',
+    'Bio': 'Best known for his cerebral, often nonlinear, storytelling, acclaimed writer-director Christopher Nolan has made a significant impact in the world of filmmaking.',
+    'Birth': 'July 30, 1970'
+  }
     },
     {
-      'Title': "Shuttler Island",
-      'Description': '',
+      'Title': 'Shutter Island',
+      'Description': 'In this psychological thriller, two U.S. Marshals investigate the disappearance of a prisoner from a mental institution, uncovering dark secrets and facing personal challenges.',
       'Genre': {
-        'Name':'',
-        'Description':''
+        'Name': 'Mystery',
+        'Description': 'Mystery films involve suspenseful and perplexing plots that engage the audience in solving a crime or uncovering hidden truths.'
       },
       'Director': {
-        'Name': '',
-        'Bio': '',
-        'Birth': ''
+        'Name': 'Martin Scorsese',
+        'Bio': 'Martin Scorsese is an American film director, producer, screenwriter, and actor. He is widely regarded as one of the greatest directors in the history of cinema.',
+        'Birth': 'November 17, 1942'
       }
     },
     {
       'Title': 'Arrival',
-      'Description': '',
-      'Genre': {
-        'Name':'',
-        'Description':''
-      },
-      'Director': {
-        'Name': '',
-        'Bio': '',
-        'Birth': ''
-      }
+  'Description': 'A linguist is recruited to help communicate with extraterrestrial visitors whose mysterious arrival sparks global tensions. As she delves into their language, she unlocks profound revelations.',
+  'Genre': {
+    'Name': 'Science Fiction',
+    'Description': 'Science fiction films explore speculative concepts, often involving advanced technology, space exploration, and extraterrestrial life.'
+  },
+  'Director': {
+    'Name': 'Denis Villeneuve',
+    'Bio': 'Denis Villeneuve is a Canadian film director and writer. Known for his visually stunning and thought-provoking films, he has gained recognition in the world of cinema.',
+    'Birth': 'October 3, 1967'
+  }
     },
     {
       'Title': 'Interstellar',
-      'Description': '',
+      'Description': 'In this science fiction epic, a team of explorers embarks on a space journey through a wormhole to find a new habitable planet for humanity facing extinction on Earth.',
       'Genre': {
-        'Name':'',
-        'Description':''
+        'Name': 'Science Fiction',
+        'Description': 'Science fiction films explore speculative concepts, often involving advanced technology, space exploration, and extraterrestrial life.'
       },
       'Director': {
-        'Name': '',
-        'Bio': '',
-        'Birth': ''
+        'Name': 'Christopher Nolan',
+        'Bio': 'Christopher Nolan continues his exploration of complex narratives and breathtaking visuals in this interstellar odyssey.',
+        'Birth': 'July 30, 1970'
       }
     },
     {
       'Title': 'Lone Survivor',
-      'Description': '',
+      'Description': 'Based on true events, this war film recounts the failed mission of a Navy SEAL team in Afghanistan and the harrowing survival story of the lone survivor.',
       'Genre': {
-        'Name':'',
-        'Description':''
+        'Name': 'War',
+        'Description': 'War films depict conflicts, battles, and the impact of war on individuals and societies.'
       },
       'Director': {
-        'Name': '',
-        'Bio': '',
-        'Birth': ''
+        'Name': 'Peter Berg',
+        'Bio': 'Peter Berg is an American director, producer, writer, and actor. He has worked on a variety of films, exploring themes of courage and resilience.',
+        'Birth': 'March 11, 1964'
       }
     },
     {
-      'Title': 'Spider-Man: Across the spider-Verse',
-      'Description': '',
+      'Title': 'Spider-Man: Across the Spider-Verse',
+      'Description': 'In this animated superhero film, Spider-Man teams up with alternate versions of himself from different dimensions to stop a powerful threat that spans across the multiverse.',
       'Genre': {
-        'Name':'',
-        'Description':''
+        'Name': 'Animation',
+        'Description': 'Animated films use visual techniques to create a captivating and imaginative storytelling experience.'
       },
       'Director': {
-        'Name': '',
-        'Bio': '',
-        'Birth': ''
+        'Name': 'Joaquim Dos Santos, Kemp Powers',
+        'Bio': 'The creative duo brings their expertise to the animated superhero world, delivering a visually stunning and dynamic Spider-Verse story.',
+        'Birth': 'Director 1 Birth Date, Director 2 Birth Date'
       }
     },
     {
       'Title': 'Howls Moving Castle',
-      'Description': '',
-      'Genre': {
-        'Name':'',
-        'Description':''
+      'Description':'A young woman is cursed with an old body by a spiteful witch. As she seeks a way to break the curse, she becomes involved with a wizard named Howl and his magical moving castle.',
+      'Genre':{
+        'Name':'Animation',
+        'Description':'Animated films are those in which individual drawings, paintings, or illustrations are photographed frame by frame (stop-frame cinematography).'
       },
       'Director': {
-        'Name': '',
-        'Bio': '',
-        'Birth': ''
-      }
-    },
-    {
-      'Title': 'Elemental',
-      'Description': '',
-      'Genre': {
-        'Name':'',
-        'Description':''
-      },
-      'Director': {
-        'Name': '',
-        'Bio': '',
-        'Birth': ''
-      }
-    },
-    {
-      'Title': 'Your Name',
-      'Description': '',
-      'Genre': {
-        'Name':'',
-        'Description':''
-      },
-      'Director': {
-        'Name': '',
-        'Bio': '',
-        'Birth': ''
+        'Name': 'Hayao Miyazaki',
+        'Bio':'Hayao Miyazaki is a Japanese animator, director, producer, screenwriter, author, and co-founder of Studio Ghibli. He is considered one of the greatest animators and directors in the history of cinema.',
+        'Birth':'January 5, 1941'
       }
     }
   ];
@@ -190,49 +172,112 @@ app.post('/users', (req, res) => {
   if (newUser.name) {
     newUser.id = uuid.v4();
     users.push(newUser);
-    res.status(201).jason(newUser);
+    res.status(201).json(newUser);
   } else {
     res.status(400).send('users need names')
   }
 })
 
+// update 
+app.put('/users/:id',(req, res) => {
+  const {id} = req.params;
+  const updatedUser = req.body;
+let user = users.find( user => user.id == id);
+
+if (user){
+  user.name = updatedUser.name;
+  res.status(200).json(user)   
+ } else {
+    res.status(400).send("User not found");
+  }
+})
+
+// create
+app.post('/users',(req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name){
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("User needs name")
+  }
+})
+
+app.post('/users/:id/:movieTitle',(req, res) => {
+  const {id, movieTitle} = req.params;
+
+let user = users.find( user => user.id == id);
+
+if (user){
+  user.favMovies.push(movieTitle);
+  res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);   
+ } else {
+    res.status(400).send("User not found");
+  }
+})
+
+//delete
+
+app.delete('/users/:id/:movieTitle',(req, res) => {
+  const {id, movieTitle} = req.params;
+
+let user = users.find( user => user.id == id);
+
+if (user){
+  user.favMovies = user.favMovies.filter(title => title !== movieTitle);
+  res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);   
+ } else {
+    res.status(400).send("User not found");
+  }
+})
+
+app.delete('/users/:id',(req, res) => {
+  const {id} = req.params;
+
+let user = users.find( user => user.id == id);
+
+if (user){
+  users = users.filter(user => user.id != id);
+  res.status(200).send(` Users ${id} has been deleted`);   
+ } else {
+    res.status(400).send("User not found");
+  }
+})
   // read movies 
   app.get('/movies', (req, res) => {
-    res.status(200).json(topMovies);
+    res.status(200).send('Movies array');
   });
 //title
   app.get('/movies/:title', (req, res) => {
-    const { title } = req.params;
-    const movie = topMovies.find( movie =>movie.Title === title);
-
-    if (movie) {
-      res.status(200).json(movie);
+    const movie = topMovies.find( movie => movie.Title === title);
+    if (movie){
+    res.status(200).json(movie);
     } else {
-      res.status(400).send('no such movie')
+      res.status(400).send("No such movie");
     }
-  })
+  });
 //genre
-  app.get('/movies/genre/:genreName', (req, res) => {
-    const { genreName } = req.params;
-    const genre = topMovies.find( movie =>movie.Genre.Name === genreName).Genre;
-
-    if (genre) {
-      res.status(200).json(genre);
-    } else {
-      res.status(400).send('no such movie')
-    }
-  })
-//director
-app.get('/movies/director/:directorName', (req, res) => {
-  const { directorName } = req.params;
-  const director = topMovies.find( movie =>movie.Director.Name === directorName).Director;
-
-  if (director) {
-    res.status(200).json(director);
+  app.get('/movies/genres/:genreName', (req, res) => {
+  const {genreName} = req.params;
+  const genre = topMovies.find( movie => movie.Genre.Name === genreName).Genre;
+  if (genre){
+  res.status(200).json(genre);
   } else {
-    res.status(400).send('no such movie')
+    res.status(400).send("No such genre");
   }
-})
+});
+//director
+app.get('/movies/directors/:directorName', (req, res) => {
+  const {directorName} = req.params;
+  const director = topMovies.find( movie => movie.Director.Name === directorName).Director;
+  if (director){
+  res.status(200).json(director);
+  } else {
+    res.status(400).send("No such director");
+  }
+});
   
   // set up error handling
 app.use((err, req, res, next)=>{
