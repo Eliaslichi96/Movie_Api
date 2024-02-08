b14a80c8c5cfb8290508e56fc22158b0d4c75493
Eliaslichi96 <146300075+Eliaslichi96@users.noreply.github.com>
2024-02-07 21:47:26 -0500

    2.10 heroku
    installed heroku belt, changed port , installed git

--- index.js
+++ index.js
@@ -7,8 +7,23 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
+const cors = require('cors');
+app.use(cors());
 const passport = require('passport');
 require('./passport');
+const { check, validationResult } = require('express-validator');
+
+// only certain origins to be given access 
+//app.use(cors({
+  //origin: (origin, callback) => {
+   // if(!origin) return callback(null, true);
+   // if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+     // let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+     // return callback(new Error(message ), false);
+   // }
+   // return callback(null, true);
+  //}
+//}));
 
 mongoose.connect('mongodb://localhost:27017/finalattempt', 
 { useNewUrlParser: true, useUnifiedTopology: true });
@@ -197,31 +212,47 @@ app.get('/users/:Username',
   Email: String,
   Birthday: Date
 }*/
-  app.post('/users',  async (req, res) => {
-    await Users.findOne({ Username: req.body.Username })
-      .then((user) => {
-        if (user) {
-          return res.status(400).send(req.body.Username + 'already exists');
-        } else {
-          Users
-            .create({
-              Username: req.body.Username,
-              Password: req.body.Password,
-              Email: req.body.Email,
-              Birthday: req.body.Birthday
-            })
-            .then((user) =>{res.status(201).json(user) })
+pp.post('/users',
+[
+  check('Username', 'Username is required').isLength({min: 5}),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()
+], async (req, res) => {
+
+// check the validation object for errors
+  let errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+    .then((user) => {
+      if (user) {
+        //If the user is found, send a response that it already exists
+        return res.status(400).send(req.body.Username + ' already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) => { res.status(201).json(user) })
           .catch((error) => {
             console.error(error);
             res.status(500).send('Error: ' + error);
-          })
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send('Error: ' + error);
-      });
-  });
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
 
 
 // Update a user's info, by username
@@ -366,6 +397,18 @@ app.get('/movies/directors/:directorName', passport.authenticate('jwt', { sessio
   }
 })
 
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // error handling
 app.use((err, req, res, next) => {
@@ -374,6 +417,7 @@ app.use((err, req, res, next) => {
 });
 
 // listen for request
-app.listen(8080, () => {
-  console.log('listening on 8080.');
-})
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
+});
